90a7a7446d10fc662b70a82444c3c4a01ca1f9b6
kodomir <korchagindm@gmail.com>
2016-11-11 01:28:28 +0300

    added bot to check flights

--- bot.py
+++ bot.py
@@ -1,84 +1,200 @@
 import sys
-import random
 import telepot
-import telepot.helper
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.delegate import (
-    per_chat_id, per_callback_query_origin, create_open, pave_event_space)
+    per_chat_id_in, per_application, call, create_open, pave_event_space)
 
 """
-$ python3.5 quiz.py <token>
-Send a chat message to the bot. It will give you a math quiz. Stay silent for
-10 seconds to end the quiz.
-It handles callback query by their origins. All callback query originated from
-the same chat message will be handled by the same `CallbackQueryOriginHandler`.
+$ python3.5 chatbox_nodb.py <token> <owner_id>
+
+Chatbox - a mailbox for chats
+
+1. People send messages to your bot.
+2. Your bot remembers the messages.
+3. You read the messages later.
+
+It accepts the following commands from you, the owner, only:
+
+- `/unread` - tells you who has sent you messages and how many
+- `/next` - read next sender's messages
+
+This example can be a starting point for **customer support** type of bots.
+For example, customers send questions to a bot account; staff answers questions
+behind the scene, makes it look like the bot is answering questions.
+
+It further illustrates the use of `DelegateBot` and `ChatHandler`, and how to
+spawn delegates differently according to the role of users.
+
+This example only handles text messages and stores messages in memory.
+If the bot is killed, all messages are lost. It is an *example* after all.
 """
 
-class QuizStarter(telepot.helper.ChatHandler):
-    def __init__(self, *args, **kwargs):
-        super(QuizStarter, self).__init__(*args, **kwargs)
+# Simulate a database to store unread messages
+class UnreadStore(object):
+    def __init__(self):
+        self._db = {}
+
+    def put(self, msg):
+        chat_id = msg['chat']['id']
+
+        if chat_id not in self._db:
+            self._db[chat_id] = []
+
+        self._db[chat_id].append(msg)
+
+    # Pull all unread messages of a `chat_id`
+    def pull(self, chat_id):
+        messages = self._db[chat_id]
+        del self._db[chat_id]
+
+        # sort by date
+        messages.sort(key=lambda m: m['date'])
+        return messages
+
+    # Tells how many unread messages per chat_id
+    def unread_per_chat(self):
+        return [(k,len(v)) for k,v in self._db.items()]
+
+
+# Accept commands from owner. Give him unread messages.
+class OwnerHandler(telepot.helper.ChatHandler):
+    def __init__(self, seed_tuple, store, **kwargs):
+        super(OwnerHandler, self).__init__(seed_tuple, **kwargs)
+        self._store = store
+
+    def _read_messages(self, messages):
+        for msg in messages:
+            # assume all messages are text
+            self.sender.sendMessage(msg['text'])
 
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        self.sender.sendMessage(
-            'Press START to do some math ...',
-            reply_markup=InlineKeyboardMarkup(
-                inline_keyboard=[[
-                    InlineKeyboardButton(text='START', callback_data='start'),
-                ]]
-            )
-        )
-        self.close()  # let Quizzer take over
 
-class Quizzer(telepot.helper.CallbackQueryOriginHandler):
-    def __init__(self, *args, **kwargs):
-        super(Quizzer, self).__init__(*args, **kwargs)
-        self._score = {True: 0, False: 0}
-        self._answer = None
+        if content_type != 'text':
+            self.sender.sendMessage("I don't understand")
+            return
 
-    def _show_next_question(self):
-        x = random.randint(1,50)
-        y = random.randint(1,50)
-        sign, op = random.choice([('+', lambda a,b: a+b),
-                                  ('-', lambda a,b: a-b),
-                                  ('x', lambda a,b: a*b)])
-        answer = op(x,y)
-        question = '%d %s %d = ?' % (x, sign, y)
-        choices = sorted(list(map(random.randint, [-49]*4, [2500]*4)) + [answer])
+        command = msg['text'].strip().lower()
 
-        self.editor.editMessageText(question,
-            reply_markup=InlineKeyboardMarkup(
-                inline_keyboard=[
-                    list(map(lambda c: InlineKeyboardButton(text=str(c), callback_data=str(c)), choices))
-                ]
-            )
-        )
-        return answer
+        # Tells who has sent you how many messages
+        if command == '/unread':
+            results = self._store.unread_per_chat()
 
-    def on_callback_query(self, msg):
-        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+            lines = []
+            for r in results:
+                n = 'ID: %d\n%d unread' % r
+                lines.append(n)
 
-        if query_data != 'start':
-            self._score[self._answer == int(query_data)] += 1
+            if not len(lines):
+                self.sender.sendMessage('No unread messages')
+            else:
+                self.sender.sendMessage('\n'.join(lines))
 
-		text = '%d out of %d' % (self._score[True], self._score[True]+self._score[False])
-		self.editor.editMessageText(text, reply_markup=None)
+        # read next sender's messages
+        elif command == '/next':
+            results = self._store.unread_per_chat()
 
-        self._answer = self._show_next_question()
+            if not len(results):
+                self.sender.sendMessage('No unread messages')
+                return
 
-    # def on__idle(self, event):
-    #     text = '%d out of %d' % (self._score[True], self._score[True]+self._score[False])
-    #     self.editor.editMessageText(text, reply_markup=None)
-    #     self.close()
+            chat_id = results[0][0]
+            unread_messages = self._store.pull(chat_id)
+
+            self.sender.sendMessage('From @' + unread_messages[0]['chat']['username'])
+            self._read_messages(unread_messages)
+
+        # else:
+            # self.sender.sendMessage("I don't understand")
+
+
+class MessageSaver(telepot.helper.Monitor):
+    def __init__(self, seed_tuple, store, exclude):
+        # The `capture` criteria means to capture all messages.
+        super(MessageSaver, self).__init__(seed_tuple, capture=[[lambda msg: not telepot.is_event(msg)]])
+        self._store = store
+        self._exclude = exclude
+
+    # Store every message, except those whose sender is in the exclude list, or non-text messages.
+    def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+
+        if chat_id in self._exclude:
+            print('Chat id %d is excluded.' % chat_id)
+            return
+
+        if content_type != 'text':
+            print('Content type %s is ignored.' % content_type)
+            return
+
+        print('Storing message: %s' % msg)
+        self._store.put(msg)
+
+
+import threading
+
+class CustomThread(threading.Thread):
+    def start(self):
+        print('CustomThread starting ...')
+        super(CustomThread, self).start()
+
+# Note how this function wraps around the `call()` function below to implement
+# a custom thread for delegation.
+def custom_thread(func):
+    def f(seed_tuple):
+        target = func(seed_tuple)
+
+        if type(target) is tuple:
+            run, args, kwargs = target
+            t = CustomThread(target=run, args=args, kwargs=kwargs)
+        else:
+            t = CustomThread(target=target)
+
+        return t
+    return f
+
+
+class ChatBox(telepot.DelegatorBot):
+    def __init__(self, token, owner_id):
+        self._owner_id = owner_id
+        self._seen = set()
+        self._store = UnreadStore()
+
+        super(ChatBox, self).__init__(token, [
+            # Here is a delegate to specially handle owner commands.
+            pave_event_space()(
+                per_chat_id_in([owner_id]), create_open, OwnerHandler, self._store, timeout=20),
+
+            # Only one MessageSaver is ever spawned for entire application.
+            (per_application(), create_open(MessageSaver, self._store, exclude=[])),
+
+            # For senders never seen before, send him a welcome message.
+            (self._is_newcomer, custom_thread(call(self._send_welcome))),
+        ])
+
+    # seed-calculating function: use returned value to indicate whether to spawn a delegate
+    def _is_newcomer(self, msg):
+        if telepot.is_event(msg):
+            return None
+
+        chat_id = msg['chat']['id']
+        if chat_id == self._owner_id:  # Sender is owner
+            return None  # No delegate spawned
+
+        if chat_id in self._seen:  # Sender has been seen before
+            return None  # No delegate spawned
+
+        self._seen.add(chat_id)
+        return []  # non-hashable ==> delegates are independent, no seed association is made.
+
+    def _send_welcome(self, seed_tuple):
+        chat_id = seed_tuple[1]['chat']['id']
+
+        print('Sending welcome ...')
+        self.sendMessage(chat_id, 'Hello!')
 
 
 TOKEN = sys.argv[1]
+OWNER_ID = int(sys.argv[2])
 
-bot = telepot.DelegatorBot(TOKEN, [
-    pave_event_space()(
-        per_chat_id(), create_open, QuizStarter, timeout=3),
-    pave_event_space()(
-        per_callback_query_origin(), create_open, Quizzer, timeout=2),
-])
-
+bot = ChatBox(TOKEN, OWNER_ID)
 bot.message_loop(run_forever='Listening ...')
---
+++ flight_bot.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import json
+import urllib2
+import telepot
+
+from telepot.delegate import per_chat_id, create_open, pave_event_space
+
+class FlightChecker(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(FlightChecker, self).__init__(*args, **kwargs)
+        self._chatId = None
+
+        while 1:
+        	self.checkFlight()
+        	time.sleep(3600)
+
+    def checkFlight(self):
+    	print('checking flight')
+
+    	if self.chat_id != None:
+        		response = urllib2.urlopen('http://www.pulkovoairport.ru/f/flights/cur/ru_dep_2.js?0.17094681738165363')
+        		data = json.load(response)
+
+        		found = False
+        		if data['data'] != None:
+        			for flight in data['data']:
+        				if flight['company'] == 'Royal Flight airlines':
+        					found = True
+        					if flight['date'] != '2016-11-12 13:00:00':
+        						print('Ooops something wrong')
+        						self.sender.sendMessage('Date changed! Panic!\nFlight date: ' + flight['date'] + '\n\n Check it! http://www.pulkovoairport.ru/')
+        					else:
+        						print('All right')
+        						self.sender.sendMessage('All right, ceep calrm.\nFlight date: ' + flight['date'])
+
+        		if found == False:
+					self.sender.sendMessage('Can\'t find flight check it. http://www.pulkovoairport.ru')
+					print('Flight not found')
+
+
+    def on_chat_message(self, msg):
+		command = msg['text'].strip().lower()
+		self.chat_id = msg['chat']['id']
+
+		if (command == '/start') or (command == '/force'):
+			self.checkFlight()
+
+        # self.sender.sendMessage()
+
+TOKEN = sys.argv[1]
+
+bot = telepot.DelegatorBot(TOKEN, [
+    pave_event_space()(
+        per_chat_id(), create_open, FlightChecker, timeout=10
+    ),
+])
+bot.message_loop(run_forever='Listening ...')
